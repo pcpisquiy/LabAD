c8027ba5dbaaa9819ffe11960ff0c89a39670089
pcpisquiy <pcpisquiy@gmail.com>
2024-04-26 00:55:49 -0600

    Creacion y lista de usuarios

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,30 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using LogicaNegocio.Sesion;
+using LogicaNegocio.DTO;
 namespace LabAD_Katas.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            var Settings = Properties.Settings.Default;
-            string CadenaConexion = FuncionesAuxiliares.Instance.DesEncripta(Settings.CadenaConexion, "DMP_2024");
             return View();
         }
 
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult ListarPacientes()
         {
-            ViewBag.Message = "Your application description page.";
-
+            GestorClinica gestorClinica = new GestorClinica(FuncionesAuxiliares.Instance.DesEncripta(Properties.Settings.Default.CadenaConexion,"DMP_2024"));
+            ViewData["ListaPaciente"] = gestorClinica.ListaPacientes();
             return View();
         }
-
-        public ActionResult Contact()
+        public ActionResult InsertarPaciente() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InsertarPacientes(string Nombre, int Edad, int Telefono)
         {
-            ViewBag.Message = "Your contact page.";
-
+            GestorClinica gestorClinica = new GestorClinica(FuncionesAuxiliares.Instance.DesEncripta(Properties.Settings.Default.CadenaConexion, "DMP_2024"));
+            gestorClinica.InsertarPaciente(new PacienteDTO() { Nombre_Paciente = Nombre, Edad = Edad, Telefono = Telefono });
             return View();
         }
+
     }
 }
