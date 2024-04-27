9cc69090737a78b3c32697eb6fb0fea0632cc2c5
pcpisquiy <pcpisquiy@gmail.com>
2024-04-26 19:54:51 -0600

    Busqueda paciente

---
+++ LogicaNegocio/DTO/SucesoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.DTO
+{
+public class SucesoDTO
+    {
+        public int idSuceso { get; set; }
+        public string suceso { get; set; }
+    }
+}
